1310fd0ddff39970fee5dadabc976cb53b199cdd
SofterM <66023096@up.ac.th>
2024-11-19 14:43:17 +0700

    fix git

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // ให้โปรเจกต์ export เป็น static
+  trailingSlash: true, // เพิ่ม / ท้าย URL สำหรับ GitHub Pages
 };
 
 export default nextConfig;
